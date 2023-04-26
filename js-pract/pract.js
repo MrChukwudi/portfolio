2902b3a1dec3e195772e8b0b70e4329b6aa88979
Desire Chukwudi <chukwudidesire01@gmail.com>
2023-04-26 13:08:47 +0100

    My first Bulk Commit

---
+++ js-pract/pract.js
@@ -0,0 +1,36 @@
+// const cars = [
+//   {
+//     model: "Honda Civic",
+//     colorsByPopularity: ["black", "Silver"],
+//     speedStats: {
+//       topSpeed: 140,
+//       zeroToSixty: 8.5,
+//     },
+//   },
+//   {
+//     model: "Tesla Model 3",
+//     colorsByPopularity: ["Red", "White"],
+//     speedStats: {
+//       topSpeed: 150,
+//       zeroToSixty: 3.2,
+//     },
+//   },
+// ];
+
+// const animal = [
+//   { name: "Cat", sound: "Meow" },
+//   { name: "Dog", sound: "Woof" },
+// ];
+
+// // Destructuring an Array:
+
+// const [cat, dog] = animal;
+// // console.log(cat);
+
+// // Destructuring an Object: We do it by pulling the keys by their names:
+
+// const { name, sound } = cat;
+
+// // console.log(name);
+
+
