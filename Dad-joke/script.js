d7adee064714208718235a7d476c7be8c2e7b79e
Desire Chukwudi <chukwudidesire01@gmail.com>
2023-05-07 01:38:28 +0100

    Finished working on the collapsible faq project

--- Dad-joke/script.js
+++ Dad-joke/script.js
@@ -1,7 +1,6 @@
 const jokeDiv = document.getElementById("joke");
 const jokeBtn = document.getElementById("jokebtn");
 
-
 jokeBtn.addEventListener("click", () => generateJoke());
 
 function generateJoke() {
@@ -13,9 +12,25 @@ function generateJoke() {
   fetch(url, myHeader)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       jokeDiv.innerHTML = data.joke;
     })
     .catch((err) => console.log(err));
 }
 
+// <<<<<<<<<<<<<<<<Using the Asynch-await approach instead of .then()>>>>>>>>>>>>>
+
+/*
+async function generateJoke(){
+  let url = "https://icanhazdadjoke.com/;
+  let myHeader = {
+    headers: { 'Content-Type': 'application/json}
+  }
+};
+
+let response = await fetch(url, myHeader) // This automatically sets the result of our fetch call to response.
+let data = await response.json(); // This will transfomr our result to a JSON object.
+jokeDiv.innerHTML = data.joke;
+
+
+
+*/
